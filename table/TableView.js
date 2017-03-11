d9c4ff86c0cf8c164332cc10a71e03a4e3673d93
Denis Golovin <denis_golovin@mail.ua>
2017-03-11 21:14:25 +0200

    done

--- table/TableView.js
+++ table/TableView.js
@@ -2,12 +2,31 @@
 	'use strict';
 
 	function TableView(table, node) {
-		console.error(table, 'TableView constructor should be implemented');
+		this.headrow = table.headers;
+		this.rows = table.rows;
 	}
 
 	TableView.prototype.createTable = function() {
-		console.error('createTable should generate table node with data');
-		return document.createElement('fake-table');
+		var tableNode = document.createElement('table');
+		var headcell = document.createElement('th');
+		var row = document.createElement('tr');
+		var cell = document.createElement('td');
+		this.headrow.forEach((head) => {
+			headcell.textContent = head;
+			row.appendChild(headcell.cloneNode(true));
+		})
+		tableNode.appendChild(row);
+		row = document.createElement('tr');
+		this.rows.forEach((rov) => {
+			rov.forEach((cel) => {
+				cell.textContent = cel;
+				row.appendChild(cell.cloneNode(true));
+			})
+			tableNode.appendChild(row.cloneNode(true));
+			row = document.createElement('tr');
+		})
+		tableNode.setAttribute("border", "2");
+		return tableNode;
 	};
 
 	window.TableView = TableView;
--- table/main.js
+++ table/main.js
@@ -1,12 +1,16 @@
-(function() {
-	'use strict';
 
-	var tableDataArea = document.querySelector('.table-data');
-	var tableMountNode = document.querySelector('.table');
-	var tableHeaders = JSON.parse(tableDataArea.value)[0];
-	var tableData = JSON.parse(tableDataArea.value)[1];
-	var table = new Table(tableHeaders, tableData);
+'use strict';
 
-	var tableView = new TableView(table);
-	tableMountNode.appendChild(tableView.createTable());
-})();
+var tableDataArea = document.querySelector('.table-data');
+var tableMountNode = document.querySelector('.table');
+var tableHeaders = JSON.parse(tableDataArea.value)[0];
+var tableData = JSON.parse(tableDataArea.value)[1];
+var table = new Table(tableHeaders, tableData);
+
+var tableViev = new TableView(table);
+var button = document.querySelector('button');
+function addNewTable() {
+	tableMountNode.innerHTML = '';
+	tableMountNode.appendChild(tableViev.createTable());
+}
+button.setAttribute('onclick', 'addNewTable()');
