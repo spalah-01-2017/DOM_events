81def5128cd69a9ef0f39699711bfc3eeca737a4
TunikDmitriy <tunik.dmitriy@gmail.com>
2017-03-12 21:07:13 +0200

    home task

--- table/main.js
+++ table/main.js
@@ -1,12 +1,19 @@
 (function() {
 	'use strict';
 
-	var tableDataArea = document.querySelector('.table-data');
-	var tableMountNode = document.querySelector('.table');
-	var tableHeaders = JSON.parse(tableDataArea.value)[0];
-	var tableData = JSON.parse(tableDataArea.value)[1];
-	var table = new Table(tableHeaders, tableData);
+	var button = document.querySelector('button');
+	button.addEventListener('click', createTable)
 
-	var tableView = new TableView(table);
-	tableMountNode.appendChild(tableView.createTable());
+	function createTable() {
+        var tableDataArea = document.querySelector('.table-data');
+        var tableMountNode = document.querySelector('.table');
+        tableMountNode.innerHTML = '';
+        var tableHeaders = JSON.parse(tableDataArea.value)[0];
+        var tableData = JSON.parse(tableDataArea.value)[1];
+        var table = new Table(tableHeaders, tableData);
+
+        var tableView = new TableView(table);
+        tableMountNode.appendChild(tableView.createTable());
+    }
+
 })();
