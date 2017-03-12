80048d69ebe59815b8b7aeedceb63a6c51800627
Ilya Kubanov <ilya-kubanov@yandex.ru>
2017-03-12 15:55:12 +0400

    dz

--- table/TableView.js
+++ table/TableView.js
@@ -1,13 +1,34 @@
 (function() {
 	'use strict';
 
-	function TableView(table, node) {
-		console.error(table, 'TableView constructor should be implemented');
+	function TableView(table) {
+		this.headers = table.headers;
+		this.rows = table.rows;
 	}
 
 	TableView.prototype.createTable = function() {
-		console.error('createTable should generate table node with data');
-		return document.createElement('fake-table');
+		var table = document.createElement('table');
+		var thead = document.createElement('thead');
+		var tbody = document.createElement('tbody');
+		var cell = document.createElement("td");
+		for(var i=0;i<this.headers.length;i++){
+			var row = document.createElement("tr");
+			var th = document.createElement("th");
+      		th.appendChild(document.createTextNode(this.headers[i]));
+			thead.appendChild(th);
+		}
+		for(var i=0;i<this.rows.length;i++){
+			var row = document.createElement("tr");
+			for(var j=0;j<this.rows[i].length;j++){
+				var td = document.createElement("td");
+				td.appendChild(document.createTextNode(this.rows[i][j]));
+				row.appendChild(td);
+			}
+			tbody.appendChild(row);
+		}
+		table.appendChild(thead);
+		table.appendChild(tbody);
+		return table;
 	};
 
 	window.TableView = TableView;
--- table/main.js
+++ table/main.js
@@ -1,12 +1,25 @@
 (function() {
 	'use strict';
+	//для того чтобы не создавать функцию отдельно и не дублировать код решил сделать так. Можно ли так делать?
+	document.addEventListener("DOMContentLoaded", create);
+	function create() {
+		var tableDataArea = document.querySelector('.table-data');
+		var tableMountNode = document.querySelector('.table');
+		var tableHeaders = JSON.parse(tableDataArea.value)[0];
+		var tableData = JSON.parse(tableDataArea.value)[1];
+		var table = new Table(tableHeaders, tableData);
+		var tableView = new TableView(table);
 
-	var tableDataArea = document.querySelector('.table-data');
-	var tableMountNode = document.querySelector('.table');
-	var tableHeaders = JSON.parse(tableDataArea.value)[0];
-	var tableData = JSON.parse(tableDataArea.value)[1];
-	var table = new Table(tableHeaders, tableData);
+		//нормально ли делать так: проверять на наличие потомка и удалять его, а затем просто заново добавлять таблицу?
+		//или лучше сделать как-то по другому?
+		if(tableMountNode.firstChild)
+			tableMountNode.removeChild(tableMountNode.firstChild);
+		tableMountNode.appendChild(tableView.createTable());
 
-	var tableView = new TableView(table);
-	tableMountNode.appendChild(tableView.createTable());
+	}
+
+	var createTable = document.querySelector('#createTable');
+	createTable.addEventListener("click", create);
+
 })();
+
