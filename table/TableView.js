17b10f88d25b93374986f456e752f81ba7d0f6df
Aleksey Kuzmenko <kurtzpb@gmail.com>
2017-03-14 19:02:40 +0200

    Finaly

--- table/TableView.js
+++ table/TableView.js
@@ -2,12 +2,32 @@
 	'use strict';
 
 	function TableView(table, node) {
-		console.error(table, 'TableView constructor should be implemented');
+		this.table = table;
 	}
 
 	TableView.prototype.createTable = function() {
-		console.error('createTable should generate table node with data');
-		return document.createElement('fake-table');
+		let table = document.createElement('table');
+		let headersRow = document.createElement('tr');
+
+		for (let i = 0; i < this.table.headers.length; i++) {
+			let header = document.createElement('th');
+			header.textContent = this.table.headers[i];
+			headersRow.appendChild(header);
+		}
+
+		table.appendChild(headersRow);
+
+		for (let i = 0; i < this.table.rows.length; i++) {
+			let row = document.createElement('tr');
+			for (let k = 0; k < this.table.headers.length; k++) {
+				let tD = document.createElement('td');
+				tD.textContent = this.table.rows[i][k] + '';
+				row.appendChild(tD);
+			}
+			table.appendChild(row);
+		}
+
+		return table;
 	};
 
 	window.TableView = TableView;
--- table/main.js
+++ table/main.js
@@ -8,5 +8,22 @@
 	var table = new Table(tableHeaders, tableData);
 
 	var tableView = new TableView(table);
+
+	var button = document.querySelector('button');
+
+	function addTableNode (e) {
+		tableDataArea = document.querySelector('.table-data');
+		tableMountNode = document.querySelector('.table');
+		tableHeaders = JSON.parse(tableDataArea.value)[0];
+		tableData = JSON.parse(tableDataArea.value)[1];
+		table = new Table(tableHeaders, tableData);
+		tableView = new TableView(table);
+
+		document.querySelector('table').remove();
+		tableMountNode.appendChild(tableView.createTable());
+	}
+
+	button.addEventListener('click', addTableNode);
+
 	tableMountNode.appendChild(tableView.createTable());
 })();
